1c77b72600055ebea8a1dafb0e7d70de3af8b5bb
Thiago Alencar <talencar@gmail.com>
2024-03-11 17:31:50 -0300

    feat: list all policies

--- app/controllers/policies_controller.rb
+++ app/controllers/policies_controller.rb
@@ -1,5 +1,11 @@
+# frozen_string_literal: true
+
+require 'net/http'
+
 class PoliciesController < ApplicationController
   def index
-    
+    uri = URI("http://policy-service:3001/v1/policies")
+    response = Net::HTTP.get_response(uri.host, uri.path, uri.port)
+    @policies = JSON.parse(response.body, symbolize_names: true)
   end
 end
